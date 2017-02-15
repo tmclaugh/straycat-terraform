113269c3b7fd661fe286ddc4e09a82a9933f9424
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-02-15 12:58:44 -0500

    Remove VPC stuff that is not in straycat-terraform-aws-vpc

--- modules/aws_vpc_peer/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_peering_connection_id" {
-  value = "${aws_vpc_peering_connection.connection.id}"
-}
-
-output "vpc_peering_connection_accept_status" {
-  value = "${aws_vpc_peering_connection.connection.accept_status}"
-}
-
